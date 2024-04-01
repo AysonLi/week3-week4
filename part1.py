1ce6dcdcc08f7c64565f619bdb879778e025b9ef
theGeekOfMayhem <ahmed.elrashidy001@gmail.com>
2024-04-01 19:31:14 -0400

    uploading code and instructions

---
+++ part1.py
@@ -0,0 +1,25 @@
+#exercise 1:
+has_homework = True
+has_free_time = False
+# Determine if can play video games
+
+#exercise 2:
+my_pets = ['dog', 'cat', 'fish', 'bird']
+# Follow the instructions to manipulate my_pets and create my_favorite_pets
+
+#exercise 3:
+phrase = "Python programming is fun!"
+# Perform the string operations
+
+#exercise 4:
+my_numbers = (1, 2, 3, 4, 5, 6)
+# Print and try to modify the tuple
+
+#exercise 5:
+student_info = {"name": "Alex", "grade_level": 10, "favorite_subject": "Math"}
+# Modify the dictionary as instructed
+
+#exercise 6:
+day = "Saturday"
+weather = "Sunny"
+# Write the conditional statements
